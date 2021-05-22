2992681334aa988fefb25ea35b84770155fd9d35
Han Verstraete <welteki@pm.me>
2021-05-22 13:18:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vanilla template
+
+Vanilla js web template using [Parcel 2](https://parceljs.org/)
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  This sandbox uses Parcel 2 to bundle and serve your app, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
